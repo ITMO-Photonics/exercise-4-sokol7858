4b9c8e7fa5ab43de65f1ee7bd57db936812e0a05
sokol7858 <36563791+sokol7858@users.noreply.github.com>
2018-03-15 01:49:39 +0300

    Add files via upload
    create scripts

--- cylinder.py
+++ cylinder.py
@@ -1,13 +1,14 @@
 ### ---------------------------------------------------------------------
-### Exercise-4: Write a Python script which calculates volume 
+### Exercise-4: Write a Python script which calculates volume
 ###       and surface area of a cylinder of radius R and length L.
 ### ---------------------------------------------------------------------
 import math # use math.pi from this module
 
-# Function for calculation of volume of a cylinder 
-def cylinder_volume(R, L):    
-#    ( your script is here) 
-  
+# Function for calculation of volume of a cylinder
+def cylinder_volume(R, L):
+    return math.pi*(R**2)*L
+
 # Function for calculation of surface area of a cylinder
 def cylinder_area(R, L):
-#    ( your script is here)
+    return 2*math.pi*(R**2)*2*math.pi*R*L
+
